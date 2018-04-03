91d1410b7d21ea8c918182cf420d15f06046d3ee
Mariusz Kurzyk <mariusz.kurzyk@gmail.com>
2018-04-03 21:57:28 +0200

    3 comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-// scripts.js
+// first comment 
 
 var name = prompt('Enter your name');
+
+/*
+The second comment 
+- long version
+*/
+
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+//3 comment
